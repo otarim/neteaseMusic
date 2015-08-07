d477623dbb2c6e93905d9f2ac32881915499ca43
otarim <otarim.com@gmail.com>
2015-08-07 18:06:23 +0800

    v2

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # neteaseMusic(又是一个网易客户端)
 
-build by angularjs + nodejs
+build by angularjs + koa
+
+好吧，公司网管禁用了一些音乐网站，可惜屏蔽手段不给力，有这个东西我又能听歌了[微笑],这几天重构下这个玩意，加多点功能（歌单。。）比较以后靠他听歌了。。。
 
 ![http://ww4.sinaimg.cn/large/9bfe0b14gw1eov5ozr2ogj20nz0i7dka.jpg](http://ww4.sinaimg.cn/large/9bfe0b14gw1eov5ozr2ogj20nz0i7dka.jpg)
 
-[http://112.74.104.132:9527/](http://112.74.104.132:9527/)
+[http://www.misaka.me/](http://www.misaka.me/)
---
+++ netease.js
@@ -0,0 +1,157 @@
+'use strict'
+var koa = require('koa'),
+    fs = require('mz/fs'),
+    path = require('path'),
+    staticServe = require('koa-static-cache'),
+    router = require('koa-router')(),
+    koaBody = require('koa-body'),
+    co = require('co'),
+    swig = require('koa-swig'),
+    queryString = require('querystring'),
+    corequest = require('co-request'),
+    crypto = require('crypto'),
+    sendfile = require('koa-sendfile'),
+    app = koa()
+
+var song = function*() {
+    var query = this.request.query
+    this.body = yield request('/api/song/detail', 'get', {
+        id: query.id,
+        ids: '[' + query.id + ']'
+    })
+}
+
+var search = function*() {
+    var body = this.request.body
+    this.body = yield request('/api/search/get/web', 'post', {
+        's': body.s,
+        'type': body.type || 1,
+        'offset': body.offset || 0,
+        'total': body.total || true,
+        'limit': 60
+    })
+}
+
+var userList = function*() {
+    var query = this.request.query
+    this.body = yield request('/api/user/playlist', 'get', {
+        offset: query.offset,
+        limit: query.limit,
+        uid: query.uid
+    })
+}
+
+var artist = function*() {
+    var query = this.request.query
+    console.log('/api/artist/artist=' + query.artist)
+    this.body = yield request('/api/artist/' + query.artist, 'get')
+}
+
+var album = function*() {
+    var query = this.request.query
+    this.body = yield request('/api/album/' + query.album, 'get')
+}
+
+var playList = function*() {
+    var query = this.request.query
+    this.body = yield request('/api/playlist/list', 'get', {
+        cat: query.category,
+        order: query.order,
+        offset: query.offset,
+        total: !!query.offset,
+        limit: query.limit
+    })
+}
+
+var playListDetail = function*() {
+    var query = this.request.query
+    this.body = yield request('/api/playlist/detail', 'get', {
+        id: query.id
+    })
+}
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(require('koa-error')())
+    app.use(require('koa-logger')())
+}
+
+
+// app.use(require('koa-charset')())
+
+//app.use(require('koa-parameter')(app))
+
+app.use(staticServe('./public/', {
+    maxAge: 365 * 24 * 60 * 60
+}))
+
+app.use(koaBody({
+    multipart: true,
+    formidable: {
+        keepExtensions: true,
+        hash: 'sha1'
+    }
+}))
+
+app.context.render = swig({
+    root: path.join(__dirname, 'view'),
+    autoescape: true,
+    cache: process.env.NODE_ENV === 'production' ? 'memory' : false,
+    ext: 'html',
+    // locals: locals,
+    // filters: filters,
+    // tags: tags,
+    // extensions: extensions
+});
+app.use(router.routes()).use(router.allowedMethods())
+
+// router
+
+router.get('/api/song', song)
+    .post('/api/search', search)
+    .get('/api/userList', userList)
+    .get('/api/artist', artist)
+    .get('/api/album', album)
+    .get('/api/playList', playList)
+    .get('/api/playListDetail', playListDetail)
+    .get('/', function*() {
+        this.body = yield this.render('index')
+    })
+
+
+app.listen(9527)
+
+var request = function*(api, method, data) {
+    var options = {
+        method: method,
+        headers: {
+            'Referer': 'http://music.163.com/',
+            'Cookie': 'appver=2.0.2;'
+        }
+    }
+    options.uri = 'http://music.163.com' + api
+    if (data) {
+        if (method === 'get') {
+            data = queryString.stringify(data)
+            options.uri += '?' + data
+        }
+        if (method === 'post') {
+            options.form = data
+        }
+    }
+    console.log(options)
+    var req = yield corequest(options)
+    return JSON.parse(req.body)
+}
+
+const api = {
+    hostname: 'music.163.com',
+    login: '/api/login/', //登陆 username: 'password': hashlib.md5( password ).hexdigest(),'rememberLogin': 'true' POST
+    userList: '/api/user/playlist', //用户歌单 offset 分页，limit 数量，uid 用户 id 
+    search: '/api/search/get/web', //搜索 's': s, 'type': stype, 'offset': offset, 'total': true/false, 'limit': 60 POST
+    playList: '/api/playlist/list', //歌单（网友精选碟 cat=' + category + '&order=' + order + '&offset=' + str(offset) + '&total=' + ('true' if offset else 'false') + '&limit=' + str(limit)
+    playListDetail: '/api/playlist/detail', //id=playListId
+    artist: '/api/artist/', //api/artist/artistId
+    album: '/api/album/',
+    song: '/api/song/detail', //id=a&ids=[a]
+    lyric: '/api/song/lyric' // id=29785473&lv=-1&kv=-1&tv=-1
+}
